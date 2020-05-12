593e6502ec03d423cbaaa207f804af588708651e
mkalluri92 <mkalluri92@gmail.com>
2020-05-12 06:37:36 -0700

    action dispatching

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ const initialState = {
 
 //reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+         return {
+             ...state,
+             counter: state.counter + 1
+         };
+    }
+    if (action.type === 'ADD_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 }
 
@@ -16,6 +28,8 @@ console.log(store.getState())
 
 
 //Dispatching Action
-
-
+//dispatch takes argument which is action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 //subscription
